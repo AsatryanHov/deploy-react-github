c5e5015954afa0bf8966f8bf151a89f8c5108949
Hov <asatryanhovhannes1995@gmail.com>
2024-07-11 15:37:06 +0200

    11.07.24

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
 
-        <p>HELLO ARMENIA 55555</p>
+        <p>HELLO ARMENIA Halo</p>
 
         <a
           className="App-link"
